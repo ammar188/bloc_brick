b23941b1f58e3a3f49b82ed7032023e82db57518
ammar <ammarhit@gmail.com>
2023-10-17 12:44:40 +0500

    added another import

--- component_global/__brick__/{{name}}/widgets/{{name}}_widget.dart
+++ component_global/__brick__/{{name}}/widgets/{{name}}_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../blocs/{{name}}_bloc/{{name.snakeCase()}}_bloc.dart';
+
 class {{name.pascalCase()}}Widget extends StatelessWidget {
   const {{name.pascalCase()}}Widget({Key? key}) : super(key: key);
 
