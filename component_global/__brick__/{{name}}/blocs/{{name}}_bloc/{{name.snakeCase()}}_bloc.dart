6f37db747a7976a5cb5c489ea4057079bc5839d8
ammar <ammarhit@gmail.com>
2023-10-17 12:41:56 +0500

    added const keywords

--- component_global/__brick__/{{name}}/blocs/{{name}}_bloc/{{name.snakeCase()}}_bloc.dart
+++ component_global/__brick__/{{name}}/blocs/{{name}}_bloc/{{name.snakeCase()}}_bloc.dart
@@ -9,7 +9,7 @@ part '{{name.snakeCase()}}_event.dart';
 part '{{name.snakeCase()}}_state.dart';
 
 class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.pascalCase()}}State> {
-  {{name.pascalCase()}}Bloc() : super({{name.pascalCase()}}State(stateEnum: {{name.pascalCase()}}StateEnum.initial)) {
+  {{name.pascalCase()}}Bloc() : super(const {{name.pascalCase()}}State(stateEnum: {{name.pascalCase()}}StateEnum.initial)) {
     on<{{name.pascalCase()}}Loaded>(_on{{name.pascalCase()}}Loaded);
   }
 
@@ -17,6 +17,6 @@ class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.
     {{name.pascalCase()}}Loaded event,
     Emitter<{{name.pascalCase()}}State> emit,
   ) async {
-    emit({{name.pascalCase()}}State(stateEnum: {{name.pascalCase()}}StateEnum.loading));
+    emit(const {{name.pascalCase()}}State(stateEnum: {{name.pascalCase()}}StateEnum.loading));
   }
 }
