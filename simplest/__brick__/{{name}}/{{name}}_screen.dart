4ffedc0a6071eab4b008c62b8dd35539629a9713
ammar <ammarhit@gmail.com>
2023-10-19 09:45:12 +0500

    removed an unnecessary import in screen

--- simplest/__brick__/{{name}}/{{name}}_screen.dart
+++ simplest/__brick__/{{name}}/{{name}}_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../blocs/{{name}}_bloc/{{name.snakeCase()}}_bloc.dart';
-
 class {{name.pascalCase()}}Screen extends StatelessWidget {
   const {{name.pascalCase()}}Screen({Key? key}) : super(key: key);
 
